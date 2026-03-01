93724ad1c7cc7f23e6f4f2c34a20ed6f98699b5d
BABAR-TAHSEEN55 <babarsmoke24@gmail.com>
2026-03-01 23:33:25 +0530

    feat: Background added

--- src/entrypoints/background/index.ts
+++ src/entrypoints/background/index.ts
@@ -1,3 +1,23 @@
-export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+// export default defineBackground(() => {
+//   console.log('Hello background!', { id: browser.runtime.id });
+// });
+
+export default defineBackground({
+  main() {
+    chrome.runtime.onInstalled.addListener(() => {
+      chrome.contextMenus.create({
+        id: "post",
+        title: "Post Insights",
+        contexts: ["all"],
+      });
+    });
+
+    chrome.runtime.onInstalled.addListener(() => {
+      chrome.contextMenus.create({
+        id: "Comments",
+        title: "Comment Insights",
+        contexts: ["all"],
+      });
+    });
+  },
 });
