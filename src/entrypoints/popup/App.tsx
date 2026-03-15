f77444b2a37b7081107d083f16c6c0398229cceb
BABAR-TAHSEEN55 <babarsmoke24@gmail.com>
2026-03-15 23:38:40 +0530

    fix: removed redundant comments

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -1,17 +1,12 @@
 import { Button } from "@/components/ui/button";
 
 const App = () => {
-  return (
-    <div className="size-[500px] bg-red-800">
-      <Button variant={"destructive"}>Click Me</Button>
-      Hello World
-    </div>
-  );
+	return (
+		<div className="size-[500px] bg-red-800">
+			<Button variant={"destructive"}>Click Me</Button>
+			Hello World
+		</div>
+	);
 };
 
 export default App;
-//TODO:: 
-//1) LLM Setup 
-//2) Dark mode Toggle ( Last Priority )
-//3) Branch llm setup maybe
-//4) Good Readme with details setup info
--- src/entrypoints/popup/main.tsx
+++ src/entrypoints/popup/main.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-// This is necessary for using Tailwind ( Tweak it according to your needs )
+// This is necessary for using Tailwind (Tweak it according to your needs)
 import "../global.css";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
