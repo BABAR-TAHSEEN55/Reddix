2fddad9a9cbe4b6f0e3f4bce38a9e740d95380dd
BABAR-TAHSEEN55 <babarsmoke24@gmail.com>
2026-03-16 02:05:21 +0530

    fix: Icons for reddix added

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,25 +2,32 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ["@wxt-dev/module-react"],
+	modules: ["@wxt-dev/module-react"],
 
-  srcDir: "src",
-  entrypointsDir: "entrypoints",
-  outDir: "dist",
-  manifest: ({ browser, manifestVersion, mode, command }) => {
-    return {
-      manifest_version: 2,
-      name: "Reddix",
-      description: "A reddit extension that solved the nightmare of subreddtis",
-      version: "1.0.0",
-      //Good to have extension permissions
-      permissions: [
-        "storage",
-        "scripting",
-        "tabs",
-        "contextMenus",
-        "activeTab",
-      ],
-    };
-  },
+	srcDir: "src",
+	entrypointsDir: "entrypoints",
+	outDir: "dist",
+	manifest: ({ browser, manifestVersion, mode, command }) => {
+		return {
+			manifest_version: 2,
+			name: "Reddix",
+			description: "A reddit extension that solved the nightmare of subreddtis",
+			version: "1.0.0",
+
+			icons: {
+				16: "reddix-16.png",
+				32: "reddix-32.png",
+				128: "reddix-128.png",
+				256: "reddix-256.png",
+			},
+			//Good to have extension permissions
+			permissions: [
+				"storage",
+				"scripting",
+				"tabs",
+				"contextMenus",
+				"activeTab",
+			],
+		};
+	},
 });
