f84cf2c4648a08e60e7f0ef914c14568dabee949
BABAR-TAHSEEN55 <babarsmoke24@gmail.com>
2026-03-16 01:40:56 +0530

    feat: Comments related action added

--- src/lib/trpc/router.ts
+++ src/lib/trpc/router.ts
@@ -1,15 +1,40 @@
 import z from "zod";
 import { AIProcedures, publicProcedures, router } from "./init";
-import { RedditPostSchema } from "../schemas";
+import { RedditCommentSchema, RedditPostSchema } from "../schemas";
 import { LLMService } from "../AIService";
 
-const llm = new LLMService()
+const llm = new LLMService();
 export const appRouter = router({
 	health: publicProcedures.query(() => "hello"),
 	post: publicProcedures.query(() => "Post Handler called"),
-	analyzePosts: AIProcedures.input(z.object({ posts: z.array(RedditPostSchema), query: z.string() })).mutation(({ input }) => {
-		return llm.smartFilterPosts(input.posts, input.query)
-	})
-})
 
-export type AppRouter = typeof appRouter
+	// Returns a filtered subset of posts (used by the Posts UI)
+	analyzePosts: AIProcedures
+		.input(z.object({ posts: z.array(RedditPostSchema), query: z.string() }))
+		.mutation(({ input }) => {
+			return llm.smartFilterPosts(input.posts, input.query);
+		}),
+
+	// Returns a natural-language response (used by LLMInterface chat UI)
+	chatAboutPosts: AIProcedures
+		.input(z.object({ posts: z.array(RedditPostSchema), query: z.string() }))
+		.mutation(({ input }) => {
+			return llm.analyzePosts(input.posts, input.query);
+		}),
+
+	// Returns a filtered subset of comments (used by the Comments UI)
+	analyzeComments: AIProcedures
+		.input(z.object({ comments: z.array(RedditCommentSchema), query: z.string() }))
+		.mutation(({ input }) => {
+			return llm.smartFilterComments(input.comments, input.query);
+		}),
+
+	// Returns a natural-language response about comments (used by a comment chat UI)
+	chatAboutComments: AIProcedures
+		.input(z.object({ comments: z.array(RedditCommentSchema), query: z.string() }))
+		.mutation(({ input }) => {
+			return llm.analyzeComments(input.comments, input.query);
+		}),
+});
+
+export type AppRouter = typeof appRouter;
