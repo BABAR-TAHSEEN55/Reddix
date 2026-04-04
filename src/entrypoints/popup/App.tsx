a29aa1c43d25ea046bdeae9f64d1807d971b400e
BABAR-TAHSEEN55 <babarsmoke24@gmail.com>
2026-04-04 21:53:15 +0530

    feat: pop fixed

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -1,12 +1,23 @@
-import { Button } from "@/components/ui/button";
+import CustomCredentials from "@/components/CustomCredentials";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-	return (
-		<div className="size-[500px] bg-red-800">
-			<Button variant={"destructive"}>Click Me</Button>
-			Hello World
-		</div>
-	);
+  return (
+    <div className="min-h-screen  bg-background text-foreground overflow-auto">
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 2800,
+          style: {
+            background: "#1f1f1f",
+            color: "#f5f5f5",
+            border: "1px solid #3a3a3a",
+          },
+        }}
+      />
+      <CustomCredentials />
+    </div>
+  );
 };
 
 export default App;
