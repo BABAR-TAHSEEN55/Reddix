f820b481d1846c9601043b7466fb3c885294f19d
BABAR-TAHSEEN55 <babarsmoke24@gmail.com>
2026-04-04 23:41:22 +0530

    fix

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,41 +2,40 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-	modules: ["@wxt-dev/module-react"],
+  modules: ["@wxt-dev/module-react"],
 
-	srcDir: "src",
-	entrypointsDir: "entrypoints",
-	outDir: "dist",
-	manifest: ({ browser, manifestVersion, mode, command }) => {
-		return {
-			manifest_version: 2,
-			name: "Reddix",
-			description: "A reddit extension that solved the nightmare of subreddtis",
-			version: "1.0.0",
+  srcDir: "src",
+  entrypointsDir: "entrypoints",
+  outDir: "dist",
+  manifest: ({ browser, manifestVersion, mode, command }) => {
+    return {
+      manifest_version: 2,
+      name: "Reddix",
+      description: "A reddit extension that solved the nightmare of subreddtis",
+      version: "1.0.0",
 
-			browser_specific_settings: {
-				gecko: {
-					data_collection_permissions: {
-						required: false,
-					},
-				},
-			},
+      browser_specific_settings: {
+        gecko: {
+          data_collection_permissions: {
+            required: ["none"],
+          },
+        },
+      },
 
-			icons: {
-				16: "reddix-16.png",
-				32: "reddix-32.png",
-				128: "reddix-128.png",
-				256: "reddix-256.png",
-			},
-			//Good to have extension permissions
-			permissions: [
-				"storage",
-				"scripting",
-				"tabs",
-				"contextMenus",
-				"activeTab",
-			],
-		};
-	},
-
+      icons: {
+        16: "reddix-16.png",
+        32: "reddix-32.png",
+        128: "reddix-128.png",
+        256: "reddix-256.png",
+      },
+      //Good to have extension permissions
+      permissions: [
+        "storage",
+        "scripting",
+        "tabs",
+        "contextMenus",
+        "activeTab",
+      ],
+    };
+  },
 });
